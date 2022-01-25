062eae30ca43053b136817591a85f87c61e36692
blady <belazdzislaw@gmail.com>
2022-01-25 23:39:49 +0100

    Refactor context

--- src/hooks/useConnections.ts
+++ src/hooks/useConnections.ts
@@ -1,60 +1,51 @@
-import { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 import { CONNECTIONS_API } from '../consts/api';
+import { get } from './get';
 
 export const useConnections = () => {
   const [isLoadingConnections, setIsLoadingConnections] = useState(false);
   const [errorConnection, setErrorConnection] = useState(false);
   const [connectionsData, setConnectionsData] = useState();
 
-  const options = {
-    url: CONNECTIONS_API,
-    headers: {
-      auth: process.env.REACT_APP_API_KEY,
-    },
-  };
-
-  const sendQuery = useCallback(async () => {
+  const sendQuery = async () => {
     try {
       setIsLoadingConnections(true);
-      const res = await axios(options);
-      setConnectionsData(res.data);
+      const response = await get(CONNECTIONS_API);
+      setConnectionsData(response.data);
     } catch (error) {
       setErrorConnection(error);
       setTimeout(() => sendQuery(), 1000);
     } finally {
       setIsLoadingConnections(false);
     }
-  }, []);
+  };
 
   useEffect(() => {
     sendQuery();
   }, []);
 
-  const convertConnectins = (connectionsData: string) => {
-    if (!connectionsData) return;
-
-    const splittedByNewLine = connectionsData.split('\n');
-
-    const convertArrayToObject = (array: string[]) => {
-      return array.reduce((obj, item) => {
-        const semicolonIndex = item.indexOf(':');
-        const key = item.slice(0, semicolonIndex);
-        const values = item.slice(semicolonIndex + 2).split(' ');
-        const clearValues = values.map((value) => value.replace(',', ''));
-
-        return {
-          ...obj,
-          [key]: clearValues,
-        };
-      }, {});
-    };
-
-    return convertArrayToObject(splittedByNewLine);
-  };
-
   const connections = convertConnectins(connectionsData);
 
   return { connections, isLoadingConnections, errorConnection };
 };
+
+const convertConnectins = (connectionsData: string) => {
+  if (!connectionsData) return;
+  const splittedByNewLine = connectionsData.split('\n');
+
+  return formatConnectionsFromApi(splittedByNewLine);
+};
+
+const formatConnectionsFromApi = (array: string[]) =>
+  array.reduce((airportsConnections, row) => {
+    const semicolonIndex = row.indexOf(':');
+    const key = row.slice(0, semicolonIndex);
+    const values = row.slice(semicolonIndex + 2).split(' ');
+    const clearValues = values.map((value) => value.replace(',', ''));
+
+    return {
+      ...airportsConnections,
+      [key]: clearValues,
+    };
+  }, {});
