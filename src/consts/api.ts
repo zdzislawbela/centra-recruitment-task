d90c285a975aefd38b295fa26ee850c120b5c71b
zdzichu <belazdzislaw@gmail.com>
2022-02-02 09:02:04 +0100

    Fix small bugs

--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,5 +1,4 @@
-export const API = 'https://centra-flights-api.herokuapp.com';
-export const AIRPORTS_API = `${API}/airports`;
-export const CONNECTIONS_API = `${API}/connections`;
+export const AIRPORTS_API = `${process.env.REACT_APP_API}/airports`;
+export const CONNECTIONS_API = `${process.env.REACT_APP_API}/connections`;
 export const LONG_WAITING_TIME = 3000;
 export const TIMEOUT = 5000;
